ac77828e159a13bab779020f2d6ac6f1e67f9f7e
Shen-Zhang <zhangshe@grinnell.edu>
2014-01-22 15:53:50 -0600

    lab1

---
+++ lab1/src/edu/grinnell/csc207/zhangshe/hello/HelloWorld.java
@@ -0,0 +1,9 @@
+package edu.grinnell.csc207.zhangshe.hello;
+
+public class HelloWorld {
+	  public static void 
+	    main (String[] args) 
+	  {
+	    System.out.println ("Hello, GitHub!");
+	  } // main(String[])
+}
