83d35e76073f0487f17bb635aabc49a721e865e2
Shen <zs112294@gmail.com>
2014-01-23 10:47:27 -0600

    Update HelloWorld.java

--- lab1/src/edu/grinnell/csc207/zhangshe/hello/HelloWorld.java
+++ lab1/src/edu/grinnell/csc207/zhangshe/hello/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	    main (String[] args) 
 	  {
 	    System.out.println ("Hello!!");
+	    System.out.println ("Hi!");
 	    System.out.println ("Hello, GitHub!!!");
 	  } // main(String[])
 }
